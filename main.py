ca7a81c3c4eb5378eb8e77198b7cdd416d76f4cd
Petrucci Lucena <petrucci.lucenaplc@gmail.com>
2024-10-23 13:27:09 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI
+from typing import List,Optional
+from pydantic import BaseModel
+from uuid import uuid4
+
+
+app = FastAPI()
+
+class Animal(BaseModel):
+    id: Optional[str]
+    nome: str
+    idade: int
+    sexo: str
+    cor: str
+
+banco: List[Animal] = []
+
+@app.get("/animais")
+
+def listar_animais():
+    return banco
+
+@app.get("/animais/{animal_id}")
+
+def listar_animal_id(animal_id: str):
+    animal = next((animal for animal in banco if animal.id == animal_id), None)
+    if animal:
+        return animal
+    return {"error": "Animal não encontrado"}
+
+
+
+@app.post("/animais/")
+
+def cadastrar_animal(animal: Animal):
+    animal.id = str(uuid4())
+    banco.append(animal)
+    return {"Animal cadastrado com sucesso !"}
+
+
+@app.delete("/animais/{animal_id}")
+
+def deletar_animal_id(animal_id: str):
+    animal = next((animal for animal in banco if animal.id == animal_id), None)
+    if animal:
+        banco.remove(animal) 
+        return {"message": "Animal deletado com sucesso!"}
+    return {"error": "Animal não encontrado"}
